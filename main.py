1afc34551b8134566a0f82b16eeade144f76d345
Camilo Aguirre <camiloaguirre24@gmail.com>
2020-04-07 19:50:19 -0500

    Handling files

---
+++ main.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+def main():
+    print('    Search a word in .txt file    ')
+
+    word = str(input('Enter the word to be searched: '))
+    find_word(word)
+
+def find_word(word):
+    counter = 0
+    with open('texto.txt') as f:
+        for line in f:
+            counter += line.count(word)
+
+    print('The word {} is in the text {} times'.format(word, counter))
+
+if __name__ == '__main__':
+    main()
